8208baff3e0f54b2dae33d0a2ea6a17a45896e8e
李聪 <827661334@qq.com>
2020-05-07 13:18:17 +0800

    no message

---
+++ AppVest.framework/Headers/AppVest.h
@@ -0,0 +1,20 @@
+//
+//  AppVest.h
+//  AppVest
+//
+//  Created by 李聪 on 2020/5/6.
+//  Copyright © 2020 李聪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Root.h"
+
+//! Project version number for AppVest.
+FOUNDATION_EXPORT double AppVestVersionNumber;
+
+//! Project version string for AppVest.
+FOUNDATION_EXPORT const unsigned char AppVestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppVest/PublicHeader.h>
+
+
---
+++ AppVest.framework/Headers/Root.h
@@ -0,0 +1,17 @@
+//
+//  Root.h
+//  AppVest
+//
+//  Created by 李聪 on 2020/5/6.
+//  Copyright © 2020 李聪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Root : NSObject
+-(void)initApp;
+@end
+
+NS_ASSUME_NONNULL_END
